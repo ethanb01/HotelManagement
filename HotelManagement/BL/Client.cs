4f7407d3a7fb5a4a787c5b347c73bc679ee5f265
ethanb01 <barkate.ethan@gmail.com>
2018-10-12 11:49:43 +0300

    update and fix taz instead of id add ID (automatic access) add function Update

--- HotelManagement/BL/Client.cs
+++ HotelManagement/BL/Client.cs
@@ -24,33 +24,42 @@ namespace HotelManagement.BL
         private string m_phone;
         public string Phone { get => m_phone; set => m_phone= value; }
 
-        private string m_id;
-        public string Id { get => m_id; set => m_id = value; }
+        private string m_taz;
+        public string Taz { get => m_taz; set => m_taz = value; }
 
         private string m_creditCard;
         public string CreditCard { get => m_creditCard; set => m_creditCard = value; }
+
+        private string m_id;
+        public string ID { get => m_id; set => m_id = value; }
 
 
 
 
         public Client() { }
 
-
         public Client(DataRow dataRow)
         {
             //מייצרת לקוח מתוך שורת לקוח
 
-            this.m_id = dataRow["IdNum"].ToString();
+            this.m_taz = dataRow["IdNum"].ToString();
             this.m_firstName= dataRow["FirstName"].ToString();
             this.m_lastName = dataRow["LastName"].ToString();
             this.m_phone= dataRow["Phone"].ToString();
             this.m_mail = dataRow["Mail"].ToString();
             this.m_creditCard = dataRow["CreditCard"].ToString();
         }
+
         public bool Insert()
         {
-            return Client_DAL.Insert(m_firstName, m_lastName, m_mail, m_id, m_phone, m_creditCard);
+            return Client_DAL.Insert(m_firstName, m_lastName, m_mail, m_taz, m_phone, m_creditCard);
         }
+
+        public bool Update()
+        {
+            return Client_DAL.Update(m_id, m_firstName, m_lastName, m_mail , m_taz, m_phone , m_creditCard);
+        }
+
         public override string ToString()
         {
             return m_firstName + " " + m_lastName;
