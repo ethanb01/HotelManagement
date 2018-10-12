bb441450933e5bb5b1cf97365dc74bda6abe4659
ethanb01 <barkate.ethan@gmail.com>
2018-10-12 10:11:16 +0300

    add function Fill: fill an array of Clients with every client from the dataTable

--- HotelManagement/BL/ClientArr.cs
+++ HotelManagement/BL/ClientArr.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Data;
+using HotelManagement.DAL;
 
 namespace HotelManagement.BL
 {
     class ClientArr : ArrayList
     {
+        public void Fill()
+        {
 
+            //להביא מה-DAL טבלה מלאה בכל הלקוחות
+
+            DataTable dataTable = Client_DAL.GetDataTable();
+
+            //להעביר את הערכים מהטבלה לתוך אוסף הלקוחות
+            //להעביר כל שורה בטבלה ללקוח
+
+            DataRow dataRow;
+            Client client;
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                dataRow = dataTable.Rows[i];
+                client = new Client(dataRow);
+                this.Add(client);
+            }
+        }
     }
 }
