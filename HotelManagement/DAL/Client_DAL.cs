db1f990214b66971403bd3cee1d8e3845bb99f92
ethanb01 <barkate.ethan@gmail.com>
2018-10-12 10:03:08 +0300

    add function that take the table_client and return the table add function FillDataSet

--- HotelManagement/DAL/Client_DAL.cs
+++ HotelManagement/DAL/Client_DAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace HotelManagement.DAL
 {
@@ -35,6 +36,21 @@ namespace HotelManagement.DAL
             + ")";
             //הפעלת פעולת הSQL -תוך שימוש בפעולה המוכנה ExecuteSql במחלקה Dal והחזרה האם הפעולה הצליחה
             return Dal.ExecuteSql(str);
+        }
+        public static DataTable GetDataTable()
+        {
+            DataTable dataTable = null;
+            DataSet dataSet = new DataSet();
+            FillDataSet(dataSet);
+            dataTable = dataSet.Tables["Table_Client"];
+            return dataTable;
+        }
+        public static void FillDataSet(DataSet dataSet)
+        {
+            //ממלאת את אוסף הטבלאות בטבלת הלקוחות
+            Dal.FillDataSet(dataSet, "Table_Client", "[LastName],[FirstName]");
+            //בהמשך יהיו כאן הוראות נוספות הקשורות לקשרי גומלין...
+
         }
     }
 }
