b7f9439c1cf61005865b952756beb4bdcdab70ec
איתן ברקת <eitan.bareket@office.eduil.org>
2018-10-26 11:43:28 +0300

    Form_city update

--- HotelManagement/UI/Form_City.Designer.cs
+++ HotelManagement/UI/Form_City.Designer.cs
@@ -28,12 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_deleteCity = new System.Windows.Forms.Button();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox_City = new System.Windows.Forms.ListBox();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_cityname = new System.Windows.Forms.TextBox();
+            this.cityname = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_deleteCity
+            // 
+            this.button_deleteCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_deleteCity.ForeColor = System.Drawing.Color.Red;
+            this.button_deleteCity.Location = new System.Drawing.Point(549, 900);
+            this.button_deleteCity.Name = "button_deleteCity";
+            this.button_deleteCity.Size = new System.Drawing.Size(267, 45);
+            this.button_deleteCity.TabIndex = 48;
+            this.button_deleteCity.Text = "DELETE CITY";
+            this.button_deleteCity.UseVisualStyleBackColor = true;
+            this.button_deleteCity.Click += new System.EventHandler(this.button_deleteCity_Click);
+            // 
+            // button_clean
+            // 
+            this.button_clean.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_clean.ForeColor = System.Drawing.Color.Coral;
+            this.button_clean.Location = new System.Drawing.Point(313, 900);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(161, 45);
+            this.button_clean.TabIndex = 47;
+            this.button_clean.Text = "Clean";
+            this.button_clean.UseVisualStyleBackColor = true;
+            this.button_clean.Click += new System.EventHandler(this.button_clean_Click);
+            // 
+            // label_ID
+            // 
+            this.label_ID.AutoSize = true;
+            this.label_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label_ID.Location = new System.Drawing.Point(279, 188);
+            this.label_ID.Name = "label_ID";
+            this.label_ID.Size = new System.Drawing.Size(36, 39);
+            this.label_ID.TabIndex = 46;
+            this.label_ID.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(21, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 39);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "ID";
+            // 
+            // listBox_City
+            // 
+            this.listBox_City.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox_City.FormattingEnabled = true;
+            this.listBox_City.ItemHeight = 31;
+            this.listBox_City.Location = new System.Drawing.Point(926, 261);
+            this.listBox_City.Name = "listBox_City";
+            this.listBox_City.Size = new System.Drawing.Size(596, 562);
+            this.listBox_City.TabIndex = 44;
+            this.listBox_City.DoubleClick += new System.EventHandler(this.listBox_City_DoubleClick);
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_cancel.Location = new System.Drawing.Point(1472, 900);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(161, 45);
+            this.button_cancel.TabIndex = 43;
+            this.button_cancel.Text = "Exit";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.Cancel_Button);
+            // 
+            // button_save
+            // 
+            this.button_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_save.ForeColor = System.Drawing.Color.Green;
+            this.button_save.Location = new System.Drawing.Point(95, 900);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(161, 45);
+            this.button_save.TabIndex = 42;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(511, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 63);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "CITY FILE";
+            // 
+            // textBox_cityname
+            // 
+            this.textBox_cityname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.textBox_cityname.Location = new System.Drawing.Point(262, 261);
+            this.textBox_cityname.Multiline = true;
+            this.textBox_cityname.Name = "textBox_cityname";
+            this.textBox_cityname.Size = new System.Drawing.Size(212, 48);
+            this.textBox_cityname.TabIndex = 40;
+            this.textBox_cityname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Check_Name);
+            // 
+            // cityname
+            // 
+            this.cityname.AutoSize = true;
+            this.cityname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.cityname.Location = new System.Drawing.Point(21, 261);
+            this.cityname.Name = "cityname";
+            this.cityname.Size = new System.Drawing.Size(189, 39);
+            this.cityname.TabIndex = 39;
+            this.cityname.Text = "City Name";
+            // 
+            // Form_City
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1654, 1033);
+            this.Controls.Add(this.button_deleteCity);
+            this.Controls.Add(this.button_clean);
+            this.Controls.Add(this.label_ID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox_City);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_cityname);
+            this.Controls.Add(this.cityname);
+            this.Name = "Form_City";
             this.Text = "Form_City";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_deleteCity;
+        private System.Windows.Forms.Button button_clean;
+        private System.Windows.Forms.Label label_ID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox_City;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_cityname;
+        private System.Windows.Forms.Label cityname;
     }
 }
--- HotelManagement/UI/Form_City.cs
+++ HotelManagement/UI/Form_City.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HotelManagement.Properties;
+using HotelManagement.BL;
+
 
 namespace HotelManagement.UI
 {
@@ -15,6 +18,169 @@ namespace HotelManagement.UI
         public Form_City()
         {
             InitializeComponent();
+            CityArrToForm();
         }
+
+
+        public bool CheckGood()
+        {
+            bool flag = true;
+            if (textBox_cityname.Text.Length < 2)
+            {
+                flag = false;
+                textBox_cityname.BackColor = Color.Red;
+            }
+           
+            return flag;
+
+        }
+
+        private void Check_Name(object sender, KeyPressEventArgs e)
+        {
+
+            if (!IsEnglishLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && (e.KeyChar != ' '))
+                e.KeyChar = char.MinValue;
+            if (CapsLockChek())
+            {
+                MessageBox.Show("CapsLock is locked");
+                e.KeyChar = char.MinValue;
+
+            }
+
+        }
+
+        public bool IsEnglishLetter(char c)
+        {
+            return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+        }
+
+        private bool CapsLockChek()
+        {
+            return (Control.IsKeyLocked(Keys.CapsLock));
+        }
+
+        public void All_White()
+        {
+            textBox_cityname.BackColor = Color.White;
+           
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            if (!CheckGood())
+            {
+                MessageBox.Show("You didn't write right", "TRY AGAIN", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                All_White();
+
+            }
+            else
+            {
+                City city = FormToCity();
+                if (city.ID == 0)
+                {
+                    CityArr oldCityArr = new CityArr();
+                    oldCityArr.Fill();
+                    if (!oldCityArr.IsContain(city.CityName))
+                    {
+                        if (city.Insert())
+                        {
+                            MessageBox.Show("City Details Saved");
+                            Clean_Form();
+                            CityArrToForm();
+
+                        }
+                        else
+                            MessageBox.Show("Cannot Save City Details");
+                    }
+                    else
+                        MessageBox.Show("City already existing");
+
+
+                }
+                else
+                {
+                    if (city.Update())
+                    {
+                        MessageBox.Show("City Details UPDATED");
+                        Clean_Form();
+                        CityArrToForm();
+
+                    }
+                    else
+                        MessageBox.Show("Cannot UPDATE City Details");
+                }
+
+
+            }
+        }
+
+        private City FormToCity()
+        {
+            City city = new City();
+            city.ID = int.Parse(label_ID.Text);
+            city.CityName = textBox_cityname.Text;
+            return city;
+        }
+
+        private void CityArrToForm()
+        {
+            //ממירה את הטנ"מ אוסף לקוחות לטופס
+            CityArr CityArr = new CityArr();
+            CityArr.Fill();
+            listBox_City.DataSource = CityArr;
+        }
+
+        private void CityToForm(City city)
+        {
+            //ממירה את המידע בטנ"מ לקוח לטופס
+            label_ID.Text = city.ID.ToString();
+            textBox_cityname.Text = city.CityName;
+        }
+
+        private void listBox_City_DoubleClick(object sender, EventArgs e)
+        {
+            CityToForm(listBox_City.SelectedItem as City);
+        }
+
+        private void Cancel_Button(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button_clean_Click(object sender, EventArgs e)
+        {
+            Clean_Form();
+        }
+
+        private void Clean_Form()
+        {
+
+            All_White();
+            textBox_cityname.Text = "";
+            label_ID.Text = "0";
+        }
+
+        private void button_deleteCity_Click(object sender, EventArgs e)
+        {
+            City city = FormToCity();
+            if (city.ID == 0)
+            {
+                MessageBox.Show("Please choice a City to delete");
+            }
+            else
+            {
+
+                if (MessageBox.Show("Are you sure to delete the city: " + city.CityName + " " + " ? ", "Be Careful", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2, MessageBoxOptions.RightAlign | MessageBoxOptions.RtlReading) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    city.Delete();
+                    Clean_Form();
+                    CityArrToForm();
+                }
+
+
+
+            }
+        }
+
     }
 }
