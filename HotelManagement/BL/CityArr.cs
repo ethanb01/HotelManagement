3b323ae0750fbc5c1eb2079de05d708966c8b5aa
איתן ברקת <eitan.bareket@office.eduil.org>
2018-10-26 10:43:52 +0300

    CityArr in BL

---
+++ HotelManagement/BL/CityArr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Data;
+using HotelManagement.DAL;
+
+namespace HotelManagement.BL
+{
+    public class CityArr : ArrayList
+    {
+        public void Fill()
+        {
+
+            //להביא מה-DAL טבלה מלאה בכל הלקוחות
+
+            DataTable dataTable = City_DAL.GetDataTable();
+
+            //להעביר את הערכים מהטבלה לתוך אוסף הלקוחות
+            //להעביר כל שורה בטבלה ללקוח
+
+            DataRow dataRow;
+            City city;
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                dataRow = dataTable.Rows[i];
+                city = new City(dataRow);
+                this.Add(city);
+            }
+        }
+    }
+}
